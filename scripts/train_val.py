102a1a2b84410e2bf46d88872e281d0f0d9ce225
Seokbin Yoon <112871410+seokbinyoon96@users.noreply.github.com>
2024-05-27 05:32:42 +0900

    Update and rename train.py to train_val.py

--- scripts/train_val.py
+++ scripts/train_val.py
@@ -18,3 +18,20 @@ def train(model, train_loader, optimizer, criterion, device, scaler):
         progress_bar.set_postfix({'Train Loss': loss.item()})
         
     return total_loss / len(train_loader)
+
+def validate(model, val_loader, criterion, device):
+    model.eval()
+    total_loss = 0
+    progress_bar = tqdm(val_loader, desc='Validating', leave=False)
+    with torch.no_grad():
+        for data, target in progress_bar:
+            data, target = data.to(device), target.to(device)
+            
+            with torch.cuda.amp.autocast():
+                output, _ = model(data)  
+                loss = criterion(output, target)
+            
+            total_loss += loss.item()
+            progress_bar.set_postfix({'Val Loss': loss.item()})
+            
+    return total_loss / len(val_loader)
